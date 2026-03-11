a096b40a765b02a059cdec08ed1f43c59c6d79b4
Danil Valiahmedov <valiahmedov2013@mail.ru>
2026-03-11 20:14:14 +0300

    VibrationSettingsInteractor напрямую влияет на VibrateClickUseCase

--- app/src/main/java/com/valimade/memorytiles/storage/di/StorageModule.kt
+++ app/src/main/java/com/valimade/memorytiles/storage/di/StorageModule.kt
@@ -10,11 +10,14 @@ import com.valimade.memorytiles.storage.data.sound.SoundKeys
 import com.valimade.memorytiles.storage.data.sound.SoundStore
 import com.valimade.memorytiles.storage.data.theme.ThemeKeys
 import com.valimade.memorytiles.storage.data.theme.ThemeStore
+import com.valimade.memorytiles.storage.data.vibration.VibrationKeys
+import com.valimade.memorytiles.storage.data.vibration.VibrationStore
 import com.valimade.memorytiles.storage.domain.display_speed.DisplaySpeedInteractor
 import com.valimade.memorytiles.storage.domain.score.ScoreInteractor
 import com.valimade.memorytiles.storage.domain.shape.ShapeInteractor
 import com.valimade.memorytiles.storage.domain.sound.SoundSettingsInteractor
 import com.valimade.memorytiles.storage.domain.theme.ThemeInteractor
+import com.valimade.memorytiles.storage.domain.vibration.VibrationSettingsInteractor
 import org.koin.dsl.module
 
 val storageModule = module {
@@ -43,5 +46,10 @@ val storageModule = module {
     single { DisplaySpeedKeys }
     single { DisplaySpeedStore(context = get(), keys = get()) }
     single { DisplaySpeedInteractor(displaySpeedStore = get()) }
+
+    //Vibration
+    single { VibrationKeys }
+    single { VibrationStore(context = get(), keys = get()) }
+    single { VibrationSettingsInteractor(vibrationStore = get()) }
 
 }
--- app/src/main/java/com/valimade/memorytiles/vibration/domain/usecase/VibrateClickUseCase.kt
+++ app/src/main/java/com/valimade/memorytiles/vibration/domain/usecase/VibrateClickUseCase.kt
@@ -1,9 +1,31 @@
 package com.valimade.memorytiles.vibration.domain.usecase
 
+import com.valimade.memorytiles.storage.domain.vibration.VibrationSettingsInteractor
 import com.valimade.memorytiles.vibration.data.entity.VibrationManager
+import com.valimade.memorytiles.vibration.domain.model.UsingVibration
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class VibrateClickUseCase(
-    private val vibrationManager: VibrationManager
+    private val vibrationManager: VibrationManager,
+    private val vibrationSettings: VibrationSettingsInteractor,
+    private val scope: CoroutineScope,
 ) {
-    fun invoke() = vibrationManager.vibrateClick()
+
+    private var isVibrationEnabled: Boolean = true
+
+    init {
+        scope.launch {
+            vibrationSettings.getUsageStatusVibration().collect { status ->
+                isVibrationEnabled = (status == UsingVibration.IS_USED)
+            }
+        }
+    }
+
+    fun invoke() {
+        if (isVibrationEnabled) {
+            vibrationManager.vibrateClick()
+        }
+    }
+
 }
