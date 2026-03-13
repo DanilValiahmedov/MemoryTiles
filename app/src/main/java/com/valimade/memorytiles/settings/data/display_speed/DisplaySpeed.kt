4a6d67211b6b1d2563e1e8015779dce136eda66e
Danil Valiahmedov <valiahmedov2013@mail.ru>
2026-03-13 18:50:36 +0300

    Добавил новый режим скорости

--- app/src/main/java/com/valimade/memorytiles/settings/data/display_speed/DisplaySpeed.kt
+++ app/src/main/java/com/valimade/memorytiles/settings/data/display_speed/DisplaySpeed.kt
@@ -5,6 +5,7 @@ enum class DisplaySpeed(
     val activeSpeed: Long,
     val inactiveSpeed: Long,
 ) {
+    VERY_SLOWLY("Очень медленно", 500, 500),
     SLOWLY("Медленно", 400, 400),
     MEASURED("Размеренно", 300, 300),
     QUICKLY("Быстро", 200, 200),
