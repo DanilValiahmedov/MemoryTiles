5fb70b165af0dc9fb36163d4f3377c699f06719c
Danil Valiahmedov <valiahmedov2013@mail.ru>
2026-03-14 21:56:17 +0300

    В DifficultyLevel добавил поле отвечающее за название уровня

--- app/src/main/java/com/valimade/memorytiles/game/domain/model/DifficultyLevel.kt
+++ app/src/main/java/com/valimade/memorytiles/game/domain/model/DifficultyLevel.kt
@@ -1,7 +1,11 @@
 package com.valimade.memorytiles.game.domain.model
 
-enum class DifficultyLevel(val sideLength: Int, val quantities: Int,) {
-    EASY(2, 4),
-    AVERAGE(3, 9),
-    DIFFICULT(4, 16),
+enum class DifficultyLevel(
+    val title: String,
+    val sideLength: Int,
+    val quantities: Int,
+) {
+    EASY("Легкий", 2, 4),
+    AVERAGE("Средний", 3, 9),
+    DIFFICULT("Сложный", 4, 16),
 }
